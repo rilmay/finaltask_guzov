53ec2849cb74dcef7b4dd87ccf3921a90507fe12
Guzov Nikita <guzovnikita@gmail.com>
2019-03-03 17:16:16 +0300

    fix 10

--- src/main/java/by/guzov/finaltask/i18n/Text.java
+++ src/main/java/by/guzov/finaltask/i18n/Text.java
@@ -46,7 +46,7 @@ public class Text extends ResourceBundle {
         String in = parent.getObject(key).toString();
         try {
             String str = new String(in.getBytes("ISO-8859-1"));
-            return str;
+            return in;
         } catch (UnsupportedEncodingException e) {
             return in;
         }
